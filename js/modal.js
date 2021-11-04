0bb2e95cbdf80f65b8e7e3935a71bb3da3d12171
Oleksii Kononenko <canon318i@gmail.com>
2021-11-04 21:49:37 +0200

    Fixed some bugs

--- js/modal.js
+++ js/modal.js
@@ -2,13 +2,15 @@
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    modal: document.querySelector('[data-modal]'),
+    burger: document.querySelector('[data-mobilemenu-btn]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+      refs.modal.classList.toggle('is-hidden');
+      refs.burger.classList.toggle('is-hidden');
   }
 })();
